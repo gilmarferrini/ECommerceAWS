726e0cf7f4b0ac2378cd16b82d4a7905b2815869
gilmarferrini <ferrinigilmar@gmail.com>
2023-02-11 20:28:40 -0300

    add lambda integration

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -3,10 +3,21 @@ import { Construct } from 'constructs'
 import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 import * as cloudWatchLogs from 'aws-cdk-lib/aws-logs'
+import { Scope } from 'aws-cdk-lib/aws-ecs'
+
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
 
 export class ECommerceApiStack extends cdk.Stack {
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
     super(scope, id, props)
+    const api = new apigateway.RestApi(this, 'ECommerceApi', {
+      restApiName: 'ECommerceApi',
+    })
+    const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+    const productsResource = api.root.addResource('products')
+    productsResource.addMethod('GET', productsFetchIntegration)
   }
 }
